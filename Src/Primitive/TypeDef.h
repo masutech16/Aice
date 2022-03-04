108bd8b8fdfabfad2d47bc2349140290fc58f75f
masutech16 <leafclover64@gmail.com>
2022-03-04 14:30:12 +0000

    数値型のtypedefを追加

---
+++ Src/Primitive/TypeDef.h
@@ -0,0 +1,11 @@
+#include <cstdint>
+
+using s32 = int_fast32_t;
+using s64 = int_fast64_t;
+
+using u32 = uint_fast32_t;
+using u64 = uint_fast64_t;
+
+using f32 = float;
+using f64 = double;
+using f128 = long double;
